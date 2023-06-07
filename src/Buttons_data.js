d557be0774fd50b395754dc21d674472e416c000
Ameen Shafeeq <ameenshafeeq2005@gmail.com>
2023-06-08 02:46:28 +0300

    hmmm

---
+++ src/Buttons_data.js
@@ -0,0 +1,133 @@
+let Buttons_data =
+    [{
+        key: 'clear',
+        class: 'AC',
+        value: 'AC',
+        func: 'clear',
+        type: 'AC'
+    }, {
+        key: 'divide',
+        value: '/',
+        class: 'operator',
+        func: 'operate',
+        type: 'operator'
+
+    }, {
+        key: 'multiply',
+        value: 'x',
+        class: 'operator',
+        func: 'operate',
+        type: 'operator'
+
+    }, {
+        key: 'seven',
+        value: '7',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+    }, {
+        key: 'eight',
+        value: '8',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+
+    }, {
+        key: 'nine',
+        value: '9',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+
+    },
+    {
+        key: 'subtract',
+        value: '-',
+        func: 'operate',
+        class: 'operator',
+        type: 'operator'
+
+
+    }, {
+        key: 'four',
+        value: '4',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+
+    }, {
+        key: 'five',
+        value: '5',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+
+    }, {
+        key: 'six',
+        value: '6',
+        func: 'write',
+        class: 'number',
+
+        type: 'number'
+
+    },
+    {
+        key: 'add',
+        value: '+',
+        func: 'operate',
+        class: 'operator',
+        type: 'operator'
+
+    }, {
+        key: 'one',
+        value: '1',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+    }, {
+        key: 'two',
+        value: '2',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+    }, {
+        key: 'three',
+        value: '3',
+        func: 'write',
+        class: 'number',
+        type: 'number'
+
+    },
+    {
+        key: 'equals',
+        value: '=',
+        func: 'equate',
+        class: 'equals',
+        type: 'equals'
+    },
+
+    {
+        key: 'zero',
+        value: '0',
+        func: 'write',
+        class: 'zero',
+        type: 'number'
+
+    }
+        ,
+    {
+        key: 'decimal',
+        value: '.',
+        func: 'write',
+        class: 'decimal',
+        type: 'number'
+
+    }]
+
+export default Buttons_data
