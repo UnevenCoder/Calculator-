a420d6f074851e96a8b07cf0c101598bedf307a8
Ameen Shafeeq <ameenshafeeq2005@gmail.com>
2023-06-15 09:11:55 +0300

    reshaped, need to work on` - `

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,31 @@ const App = () => {
     const [equals, seteq] = useState(false)
     const [deci, usedeci] = useState(false)
     function karo(val, type) {
-        makeformula(val, type)
+
+        if (type == 'number' && val != '.') {
+            Numb(val)
+        }
+        else if (type == 'number' && val == '.') {
+            Numbdeci(val)
+        }
+        else if (type == 'operator') {
+            Operate(val)
+        }
+        else if (type == 'equals') {
+            Equate()
+        }
+        else if (type == 'AC') {
+            Clear()
+        }
     }
 
-    function makeformula(val, type) {
-        if (type == 'number' && formula.length > 0) {
-            if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '-' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != 'x' && equals == false) {
+    function Numb(val) {
+        if (formula.length) {
+            if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != 'x' && equals == false) {
                 setformula((formula) => formula.map((f, index) => {
                     if (index == formula.length - 1) {
                         if (val != '.') { return (formula[formula.length - 1] + val) }
-                        else if (deci == false) { return (formula[formula.length - 1] + val, usedeci(true)) }
+                        else if (val == '.' && deci == false) { usedeci(true); console.log(formula[formula.length - 1] + val); return (formula[formula.length - 1] + val) }
                     }
                     else {
                         return formula[index]
@@ -28,30 +43,156 @@ const App = () => {
                 setoutput(+(formula[formula.length - 1] + val))
             }
             else if (equals == false) {
-
                 setformula((arr) => [...arr, val])
                 setoutput(+val)
-
             }
         }
-        else if (type == 'number') {
-            setformula((arr) => [val])
-            setoutput(+val)
-            console.log(val)
+        else {
+            setformula(() => [val])
+            setoutput(val)
         }
-        else if (type == 'operator' && formula.length > 0) {
+    }
 
-            if (equals == false) {
-                if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '-' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != '*') {
+    function Numbdeci(val) {
+        if (formula.length) {
+            if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '/' && equals == false && deci == false) {
+                setformula((formula) => formula.map((f, index) => {
+                    if (index == formula.length - 1) {
+                        if (val == '.') { usedeci(true); return (formula[formula.length - 1] + val) }
+                    }
+                    else {
+                        return formula[index]
+                    }
+                }, []))
 
+                setoutput(+(formula[formula.length - 1] + val))
+                usedeci(true)
+            }
+        }
+        else {
+            setformula(() => ['0.'])
+            setoutput(0.)
+            usedeci(true)
+        }
+    }
+
+    function Operate(val) {
+        if (formula.length) {
+            if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '-' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != 'x') {
+                setformula((arr) => [...arr, val])
+                setoutput(val)
+                usedeci(false)
+            }
+            else {
+                if (formula[formula.length - 1] != '-') {
                     setformula((arr) => [...arr, val])
-                    setoutput()
+                    setoutput(val)
                     usedeci(false)
                 }
                 else {
                     setformula((formula) => formula.map((f, index) => {
                         if (index == formula.length - 1) {
+                            if (val == '+' || val == 'x' || val == '/') { return (formula[formula.length - 1] = val) }
+                        }
+                        else {
+                            return formula[index]
+                        }
+                    }, []))
+                }
+            }
+        }
+        else if (val == '-' && formula.length == 0) {
+            setformula(() => ['-'])
+        }
+    }
 
+    function Equate() {
+        if (formula.length > 0) {
+            let result
+            let operator = ''
+            for (let i = 0; i < formula.length; i++) {
+                if (+formula[i]) {
+                    if (operator == '') {
+                        result = +formula[i]
+                    }
+                    else {
+                        if (operator == 'x') {
+                            result = Number(result) * Number(formula[i])
+                        }
+                        else if (operator == '/') {
+                            result = Number(result) / Number(formula[i])
+                        }
+                        else if (operator == '+') {
+                            result = Number(result) + Number(formula[i])
+                        }
+                        else if (operator == '-') {
+                            result = Number(result) - Number(formula[i])
+                        }
+                        i
+                    }
+                }
+                else {
+                    console.log(formula[i])
+                    operator = formula[i]
+                }
+                console.log(result)
+            }
+            setoutput(result)
+            setformula(formula => [...formula, '=' + result])
+            seteq(true)
+        }
+    }
+
+
+    function Clear() {
+        setformula([])
+        setoutput(0)
+        seteq(false)
+        usedeci(false)
+    }
+
+    /*function makeformula(val, type) {
+    
+        if (type == 'number' && formula.length > 0) {
+    
+            if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '-' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != 'x' && equals == false && deci == false) {
+                setformula((formula) => formula.map((f, index) => {
+                    if (index == formula.length - 1) {
+                        if (val != '.') { return (formula[formula.length - 1] + val) }
+                        else if (val == '.' && deci == false) { usedeci(true); console.log(formula[formula.length - 1] + val); return (formula[formula.length - 1] + val) }
+                    }
+                    else {
+                        return formula[index]
+                    }
+                }, []))
+    
+                setoutput(+(formula[formula.length - 1] + val))
+            }
+            else if (equals == false) {
+                usedeci(false)
+                setformula((arr) => [...arr, val])
+                setoutput(+val)
+    
+            }
+        }
+        else if (type == 'number' && val != '.') {
+            setformula(() => [val])
+            setoutput(+val)
+            console.log(val)
+        }
+        else if (type == 'operator' && formula.length > 0) {
+            usedeci(false)
+    
+            if (equals == false) {
+                if (formula[formula.length - 1] != '+' && formula[formula.length - 1] != '/' && formula[formula.length - 1] != '*') {
+    
+                    setformula((arr) => [...arr, val])
+                    setoutput()
+                }
+                else {
+                    setformula((formula) => formula.map((f, index) => {
+                        if (index == formula.length - 1) {
+    
                             return (
                                 val)
                         }
@@ -59,7 +200,6 @@ const App = () => {
                             return formula[index]
                         }
                     }, []))
-                    usedeci(false)
                     setoutput()
                 }
             }
@@ -68,15 +208,14 @@ const App = () => {
                 seteq(false)
                 setoutput()
                 console.log(equals)
-
+    
             }
-
+    
         }
         else if (type == 'AC') {
-            setformula([])
-            setoutput(0)
+    
         }
-
+    
         else if (type == 'equals') {
             if (formula.length > 0) {
                 let result
@@ -113,9 +252,9 @@ const App = () => {
                 seteq(true)
             }
         }
-
+    
     }
-
+    */
     return (
         <div id='body'>
             <section id="calculator">
@@ -135,6 +274,7 @@ const App = () => {
         </div>
     )
 }
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
